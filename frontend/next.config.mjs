cc4f5a4493bb83c68ce2676fb2ec53184646b886
parth <plathiya2611@gmail.com>
2024-11-19 12:13:24 +0530

    update nextjs config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Other existing configurations...
+  typescript: {
+    // ⚠️ WARNING:
+    // Setting this to true allows production builds to successfully complete
+    // even if your project has type errors.
+    ignoreBuildErrors: true,
+  },
+};
 
 export default nextConfig;
-
-// next.config.js
-module.exports = {
-    // Other existing configurations...
-    typescript: {
-      // ⚠️ WARNING:
-      // Setting this to true allows production builds to successfully complete
-      // even if your project has type errors.
-      ignoreBuildErrors: true,
-    },
-  };
-  
